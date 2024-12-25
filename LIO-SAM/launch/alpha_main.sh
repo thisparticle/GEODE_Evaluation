4e25a5446b0b7ecc99e03db188c88db878e749da
thisparticle <yunpang000@126.com>
2024-12-25 16:30:20 +0800

    add LIO-SAM

---
+++ LIO-SAM/launch/alpha_main.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# ours
+roslaunch lio_sam alpha.launch \
+    bag_file:=$data_path \
+    # output_path:=$result_path
+# sleep 10
+
---
+++ LIO-SAM/launch/carol_main.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# ours
+roslaunch lio_sam carol.launch \
+    bag_file:=$data_path \
+    # output_path:=$result_path
+# sleep 10
+
+
