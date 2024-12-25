0b09c5a9fdd5e51f178bbdf12f1e481920c05b19
thisparticle <yunpang000@126.com>
2024-12-25 16:30:57 +0800

    add LVI-SAM

---
+++ LVI-SAM/launch/bob_main.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# ours
+roslaunch lvi_sam bob.launch \
+    bag_file:=$data_path \
+    # output_path:=$result_path
+# sleep 10
+
+
---
+++ LVI-SAM/launch/carol_main.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# ours
+roslaunch lvi_sam carol.launch \
+    bag_file:=$data_path \
+    # output_path:=$result_path
+# sleep 10
+
